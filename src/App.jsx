49c2f18d389919ccc2162bf0f73bdc6fa5916ef2
Pabmoy25 <pabmoy1725@gmail.com>
2024-03-29 15:01:06 -0300

    Se agrego logo y menu inicial de navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import Footer from './components/common/Footer'
 import Menu from './components/common/Menu'
 import PaginaPrincipal from './components/pages/PaginaPrincipal'
--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,10 +1,27 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import logoHotel from "../../assets/logoHotel.png";
+
+
+
 
 const Menu = () => {
     return (
-        <div>
-            <h1>Aqui va el navbar del proyecto</h1>
-        </div>
-    );
-};
+        <Navbar expand="lg" className="bg-body-tertiary">
+          <Container>
+            <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" width={150}></img>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link href="#home">Inicio</Nav.Link>
+                <Nav.Link href="#link">Nosotros</Nav.Link>
+                <Nav.Link href="#link">Administrador</Nav.Link>
+                <Nav.Link href="#link">Login</Nav.Link>
+                <Nav.Link href="#link">Registro</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
+    }
 
 export default Menu;
