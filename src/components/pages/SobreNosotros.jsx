48105f993c8053e633b040a1a013f6e5be4d3d58
Antonella Ortega <antonellaportega@gmail.com>
2024-04-07 22:50:31 -0300

    .

---
+++ src/components/pages/SobreNosotros.jsx
@@ -0,0 +1,15 @@
+import Menu from "../common/Menu";
+import Footer from "../common/Footer";
+
+
+const SobreNosotros = () => {
+    return (
+        <>
+        <Menu></Menu>
+            <h1>Página de Nosotros</h1>
+        <Footer></Footer>
+        </>
+    );
+};
+
+export default SobreNosotros;
