17fdd041d4e96bbf648fa0fd6f1001e3404f85aa
Antonella Ortega <antonellaportega@gmail.com>
2024-04-07 23:22:53 -0300

    creamos eel texto principal de la pagina

--- src/components/pages/SobreNosotros.jsx
+++ src/components/pages/SobreNosotros.jsx
@@ -2,11 +2,22 @@ import Menu from "../common/Menu";
 import Footer from "../common/Footer";
 import { Link } from "react-router-dom";
 
-
 const SobreNosotros = () => {
     return (
         <>
-            <h1>Página de Nosotros</h1>
+         <section>
+            <h1 class="mt-4 tituloAdmin text-center">Acerca de Nosotros</h1>
+        </section>
+        <section class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 col-lg-12 col-sm-12 mb-sm-4">
+              <h2 class="texto-principal">Bienvenido a Haku Wasi - Tu Refugio de Cinco Estrellas</h2>
+              <p>
+              En Haku Wasi, nos enorgullece ofrecerte una experiencia excepcional de relajación y descanso en un entorno de lujo incomparable. Situado en medio de exuberantes paisajes naturales, nuestro hotel boutique de cinco estrellas está diseñado para ser tu refugio ideal lejos del bullicio de la vida cotidiana. Desde nuestro impecable servicio hasta nuestras instalaciones de primer nivel, todo en Haku Wasi está cuidadosamente diseñado para garantizar que cada momento de tu estancia sea inolvidable.
+              </p>
+            </div>
+          </div>
+        </section>
         </>
     );
 };
