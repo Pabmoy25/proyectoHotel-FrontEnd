7dde2e3d68a7aa04d30904936c6d2130da953e35
Antonella Ortega <antonellaportega@gmail.com>
2024-04-09 23:56:53 -0300

    cree  la galera movile, donde hay cinco imagenes del hotel

--- src/components/pages/GaleriaImagenes.jsx
+++ src/components/pages/GaleriaImagenes.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import logoHotel from '../../assets/logoHotel.png'
 
 const GaleriaImagenes = () => {
     return (
-        <div>
-            <h1>prueba</h1>
+        <>
+         <div className='galeria-moviles container'>
+         <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" ></img>
+         <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" ></img>
+         <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" ></img>
+         <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" ></img>
+         <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" ></img>
         </div>
+        </>
+       
     );
 };
 
