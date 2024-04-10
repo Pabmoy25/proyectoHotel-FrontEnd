bccd5ec983c0ea5f9a8ea4ed19e0799fc4875cdc
Julián Toranzo Navarro <julianalmacenamiento20@gmail.com>
2024-04-10 16:38:49 -0300

    corregida la ruta para que el link a inicio de error 404 lleve a la página principal

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importa el componente Link
 
 const Error404 = () => {
   return (
     <section>
       <h1>Error 404: Página no encontrada</h1>
       <p>Lo sentimos, la página que estás buscando no existe.</p>
-      <p>Vuelve al <a href="./">inicio</a>.</p>
+      {/* Utiliza Link para crear el enlace */}
+      <p>Vuelve al <Link to="/">inicio</Link>.</p>
     </section>
   );
 }
 
 export default Error404;
+
