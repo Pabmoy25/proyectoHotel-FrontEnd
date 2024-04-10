800332fca95a2c2a01595b35bb2ab704ffbd9b1f
Antonella Ortega <antonellaportega@gmail.com>
2024-04-10 01:22:49 -0300

    agregue los campos necesarios para el formulario

--- src/components/pages/Registro.jsx
+++ src/components/pages/Registro.jsx
@@ -11,21 +11,47 @@ const Registro = () => {
          <div class="form-box">
             <div class="form-value">
                 <form action="">
-                    <h2 className='titulo-registro'>Registrarse</h2>
+                    <h2 className='titulo-registro text-center'>Registrarse</h2>
 
+                    <div className="form-group row">
+                                <div className="col">
+                                <div class="inputboxAyN">
+                        <input type="Nombre" required></input>
+                        <label for="">Nombre</label>
+                    </div>
+                                </div>
+                                <div className="col">
+                                <div class="inputboxAyN">
+                        <i className="fa-regular fa-envelope icon"></i>
+                        <input type="Apellido" required></input>
+                        <label for="">Apellido</label>
+                    </div>
+
+                                </div>
+                            </div>
                     <div class="inputbox">
-                        <i class="fa-regular fa-envelope icon"></i>
+                        <i className="fa-regular fa-envelope icon"></i>
                         <input type="email" required></input>
                         <label for="">Email</label>
                     </div>
 
                     <div class="inputbox">
+                        <i class="fa-regular fa-envelope icon"></i>
+                        <input type="email" required></input>
+                        <label for="">Repetir Email</label>
+                    </div>
+                    <div class="inputbox">
                         <i class="fa-solid fa-lock icon"></i>
                         <input type="password" required></input>
-                        <label for="">Password</label>
+                        <label for="">Contraseña</label>
                     </div>
 
-
+                    <div class="inputbox">
+                        <i class="fa-solid fa-lock icon"></i>
+                        <input type="password" required></input>
+                        <label for="">Repetir Contraseña</label>
+                    </div>
+            
                     <button>Registrar</button>
 
                 </form>
