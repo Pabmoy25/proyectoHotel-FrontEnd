f5e949ccb078b079b82e99aaf36d5b8117d3427d
Agostina Bellotti <agostinabell@gmail.com>
2024-04-10 06:06:42 -0300

    Se agregó una sección nueva debajo del carrusel.

--- src/components/pages/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/components/pages/PaginaPrincipal/PaginaPrincipal.jsx
@@ -6,7 +6,8 @@ const PaginaPrincipal = () => {
     return (
         <>
         <SliderCarrusel/>
-        
+
+        <section>
         <div className="image-section">
         <img src="assets/main-image.jpeg" alt="Main Image" />
         <aside>
@@ -14,26 +15,8 @@ const PaginaPrincipal = () => {
           <h4>SOMOS PET-FRIENDLY</h4>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
         </aside>
-      </div>
- 
-        <div className="card-section">
-       
-        <div className="card">
-        <img src="assets/main-image.jpeg" alt="Main Image" />
-          <h2>Card 1</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-        </div>
-        <div className="card">
-        <img src="assets/main-image.jpeg" alt="Main Image" />
-          <h2>Card 2</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-        </div>
-        <div className="card">
-        <img src="assets/main-image.jpeg" alt="Main Image" />
-          <h2>Card 3</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.</p>
-        </div>
-      </div>
+      </div> 
+        </section>
     </>
     )
 };
