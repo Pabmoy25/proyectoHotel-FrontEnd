0a8f2872e1dacc79aceea7e39f0465c861e5ea2d
Pabmoy25 <pabmoy1725@gmail.com>
2024-04-12 22:06:12 -0300

    se modifica el logo del proyecto

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -5,20 +5,20 @@ import { Link, NavLink } from "react-router-dom";
 const Menu = () => {
     return (
         
-        <Navbar expand="lg" className="bg-violeta">
+        <Navbar expand="lg">
           <Container>
             <NavbarBrand as={Link} to={"/"} >
-            <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" width={130}></img>
+            <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" width={160}></img>
             </NavbarBrand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                <NavLink end className=" nav-link text-light" to={'/'}>Inicio</NavLink>
-                <NavLink end className="nav-link text-light" to={'/nosotros'}>Nosotros</NavLink>
-                <NavLink end className="nav-link text-light" to={'/error404'}>Contacto</NavLink>
-                <NavLink end className="nav-link text-light" to={'/administrador'}>Administrador</NavLink>
-                <NavLink end className="nav-link text-light" to={'/login'}>Login</NavLink>
-                <NavLink end className="nav-link text-light" to={'/registro'}>Registro</NavLink>
+                <NavLink end className="nav-link" to={'/'}>Inicio</NavLink>
+                <NavLink end className="nav-link" to={'/nosotros'}>Nosotros</NavLink>
+                <NavLink end className="nav-link" to={'/error404'}>Contacto</NavLink>
+                <NavLink end className="nav-link" to={'/administrador'}>Administrador</NavLink>
+                <NavLink end className="nav-link" to={'/login'}>Login</NavLink>
+                <NavLink end className="nav-link" to={'/registro'}>Registro</NavLink>
               </Nav>
             </Navbar.Collapse>
           </Container>
