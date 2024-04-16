7ca5163d209fc25811825c9498cde1d143652c5b
Agostina Bellotti <agostinabell@gmail.com>
2024-04-16 14:12:57 -0300

    Se modificaron las imagenes y se las importó dentro del componente CardsInfo y se les agregó algo de estilo a las cars que las contiene

--- src/components/pages/PaginaPrincipal/CardOK.jsx
+++ src/components/pages/PaginaPrincipal/CardOK.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import IconoFood from '../../../assets/ImgPagPrincipal/IconoFood.svg';
-import IconoLugar from '../../../assets/ImgPagPrincipal/IconoLugar.svg';
-import IconoWifi from '../../../assets/ImgPagPrincipal/IconoWifi.svg';
-import IconoGym from '../../../assets/ImgPagPrincipal/IconoGym.svg';
-import IconoPerson from '../../../assets/ImgPagPrincipal/IconoPerson.svg';
-import IconoAuto from '../../../assets/ImgPagPrincipal/IconoAuto.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 const CardOK = ({title, imageURL}) => {
     return (
         <div className='card text-center'>
-            <img src={imageURL} alt=''/>
+            <img src={imageURL} width="200" height="200"  alt=''/>
             <div className='card-body'>
-            <h4 className='card-title'>{title}</h4>
-            <p className='card-text text secondary text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <h4 className='card-title text-center'>{title}</h4>
+           
             </div>
             
         </div>
--- src/components/pages/PaginaPrincipal/CardsInfo.jsx
+++ src/components/pages/PaginaPrincipal/CardsInfo.jsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import CardOK from './CardOK';
 import { Card } from 'react-bootstrap';
-import IconoFood from '../../../assets/ImgPagPrincipal/IconoFood.svg';
+import IconoFood from '../../../assets/ImgPagPrincipal/Icono_Food.png';
+import IconoLugar from '../../../assets/ImgPagPrincipal/Icono_Lugar.png';
+import IconoWifi from '../../../assets/ImgPagPrincipal/Icono_Wifi.png';
+import IconoGym from '../../../assets/ImgPagPrincipal/Icono_Gym.png'
+import IconoPerson from '../../../assets/ImgPagPrincipal/Icono_Person.png';
+import IconoAuto from '../../../assets/ImgPagPrincipal/Icono_Auto.png';
+
 
 
 const ArrayCards = [
   {
     id: 1,
-    image: 'https://via.placeholder.com/150',
+    image: IconoFood,
     title: 'Gastronomía',
   },
   {
     id: 2,
-    image: 'https://via.placeholder.com/150',
+    image: IconoLugar,
     title: 'Espacios',
   },
   {
     id: 3,
-    image: 'https://via.placeholder.com/150',
+    image: IconoWifi,
     title: 'Wifi',
   },
   {
     id: 4,
-    image: 'https://via.placeholder.com/150',
+    image: IconoGym,
     title: 'Gym',
   },
   {
     id: 5,
-    image: 'https://via.placeholder.com/150',
+    image: IconoPerson,
     title: 'Personal',
   },
   {
     id: 6,
-    image: 'https://via.placeholder.com/150',
+    image: IconoAuto,
     title: 'Traslados',
   },
 
@@ -42,7 +48,7 @@ const ArrayCards = [
 
 function CardsInfo () {
   return (
-    <div className='container'>
+    <div className='container d-flex justify-content-center w-100'>
       <div className='row'>
         {
           ArrayCards.map(card => (
