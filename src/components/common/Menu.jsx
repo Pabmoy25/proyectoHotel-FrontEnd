c139ce55e85bf28d9267d5e0e25b5ca58de823a7
fatiperea <fatimaperea3pppp0@gmail.com>
2024-04-17 17:31:20 -0300

    agrego logica para logout y navegacion

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -2,30 +2,58 @@ import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
 import logoHotel from "../../assets/logoHotel.png";
 import { Link, NavLink } from "react-router-dom";
 
+const Menu = ({ logueado, setLogueado }) => {
 
-const Menu = () => {
-    return (
-        
-        <Navbar expand="lg" className="menu">
-          <Container>
-            <NavbarBrand as={Link} to={"/"} >
-            <img src={logoHotel} alt="logo Hotel Haki Wasi" className="img-fluid" width={170}></img>
-            </NavbarBrand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <NavLink end className="nav-link" to={'/'}>Inicio</NavLink>
-                <NavLink end className="nav-link" to={'/nosotros'}>Nosotros</NavLink>
-                <NavLink end className="nav-link" to={'/error404'}>Contacto</NavLink>
-                <NavLink end className="nav-link" to={'/administrador'}>Administrador</NavLink>
-                <NavLink end className="nav-link" to={'/galeria'}>Galeria</NavLink>
-                <NavLink end className="nav-link" to={'/login'}>Login</NavLink>
-                <NavLink end className="nav-link" to={'/registro'}>Registro</NavLink>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      );
-    }
+  const navegacion = useNavigate();
+
+  const logout = () => {
+    sessionStorage.removeItem("usuarioHakuHuasi");
+
+    setLogueado({});
+
+    navegacion("/");
+  };
+
+  return (
+    <Navbar expand="lg" className="menu">
+      <Container>
+        <NavbarBrand as={Link} to={"/"}>
+          <img
+            src={logoHotel}
+            alt="logo Hotel Haki Wasi"
+            className="img-fluid"
+            width={170}
+          ></img>
+        </NavbarBrand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end className="nav-link" to={"/"}>
+              Inicio
+            </NavLink>
+            <NavLink end className="nav-link" to={"/nosotros"}>
+              Nosotros
+            </NavLink>
+            <NavLink end className="nav-link" to={"/error404"}>
+              Contacto
+            </NavLink>
+            <NavLink end className="nav-link" to={"/administrador"}>
+              Administrador
+            </NavLink>
+            <NavLink end className="nav-link" to={"/galeria"}>
+              Galeria
+            </NavLink>
+            <NavLink end className="nav-link" to={"/login"}>
+              Login
+            </NavLink>
+            <NavLink end className="nav-link" to={"/registro"}>
+              Registro
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Menu;
