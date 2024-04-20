d50317cd7bfa8d160146ff5301053590e41d50ca
Pabmoy25 <pabmoy1725@gmail.com>
2024-04-20 13:07:53 -0300

    se soluciona error en SliderCarrusel por la key faltante

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -5,15 +5,21 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="text-center bg-violeta text-light py-4">
+    <footer className="bg-violeta text-light py-4">
       <div className="container">
-        <section className="row align-items-center">
+        <section className="row">
           <div className="col-lg-3 mt-3 d-flex flex-column align-items-center">
             <h4>Hotel Haku Huasi</h4>
-            <p>Dirección: Ruta 340 - Km 23, San Quintín</p>
-            <p>CP: 4101</p>
-            <p>Localidad: San Quintín, Yerba Buena</p>
-            <p>Provincia: País: Argentina</p>
+            <p><span className="sutbtitulofooter">Dirección:</span> Ruta 340 - Km 23, San Quintín</p>
+            <p><span className="sutbtitulofooter">Codigo Postal:</span> 4101</p>
+            <p><span className="sutbtitulofooter">Localidad:</span> San Quintín, Yerba Buena</p>
+            <p><span className="sutbtitulofooter">Provincia:</span> País: Argentina</p>
+            <br />
+            <h4>Contacto en Argentina</h4>
+            <p><span className="sutbtitulofooter">Tel./Fax:</span> (+54 9) (0381) 4924444 / 4925555</p>
+            <p><span className="sutbtitulofooter">Móvil:</span>(+54 9) (0381) 155-333333</p>
+            <p><span className="sutbtitulofooter">e-mail: </span> hakuhuasihotel@gmail.com.ar</p>
+           
           </div>
           <div className="col-lg-3 mt-3 d-flex flex-column align-items-center">
             <h4>Seguínos</h4>
@@ -72,14 +78,14 @@ const Footer = () => {
               <img
                 src={logoTripAdvisor}
                 alt="logo de trip advisor"
-                className="img-fluid me-lg-3"
+                className="img-fluid"
                 width={120}
               />
             </Link>
           </div>
         </section>
       </div>
-      <p>&copy; Todos los derechos reservados</p>
+      <p className="text-center">&copy; Todos los derechos reservados</p>
     </footer>
   );
 };
