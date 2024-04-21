9e88adf106377e047a92aa3792c2315c68beb39b
Anto Ortega <antonellaportega@gmail.com>
2024-04-21 16:17:57 -0300

    agregue estilos al registro

--- src/components/pages/Registro.jsx
+++ src/components/pages/Registro.jsx
@@ -3,39 +3,37 @@ import Fondo from "../../assets/Registr.png";
 
 const Registro = () => {
   return (
-    <form class="form-registro">
-      <p class="title-registro">Register </p>
-      <p class="message-registro">Signup now and get full access to our app. </p>
-      <div class="flex-registro">
-        <label>
-          <input class="input-registro" type="text" placeholder="" required=""></input>
-          <span>Firstname</span>
-        </label>
-
-        <label>
-          <input class="input-registro" type="text" placeholder="" required=""></input>
-          <span>Lastname</span>
-        </label>
-      </div>
-
-      <label>
-        <input class="input-registro" type="email" placeholder="" required=""></input>
-        <span>Email</span>
-      </label>
-
-      <label>
-        <input class="input-registro" type="password" placeholder="" required=""></input>
-        <span>Password</span>
-      </label>
-      <label>
-        <input class="input-registro" type="password" placeholder="" required=""></input>
-        <span>Confirm password</span>
-      </label>
-      <button class="submit-registro">Submit</button>
-      <p class="signin-registro">
-        Already have an acount ? <a href="#">Signin</a>{" "}
-      </p>
-    </form>
+    <div class="container-registro">
+    <div class="form_area-registro">
+        <p class="title-registro">REGISTRATE</p>
+        <form action="">
+            <div class="form_group-registro">
+                <label class="sub_title-registro" for="name">Nombre</label>
+                <input placeholder="Juan" class="form_style-registro" type="text" required=""></input>
+            </div>
+            <div class="form_group-registro">
+                <label class="sub_title-registro" for="name">Apellido</label>
+                <input placeholder="Perez" class="form_style-registro" type="text" required=""></input>
+            </div>
+            <div class="form_group-registro">
+                <label class="sub_title-registro" for="email">Email</label>
+                <input placeholder="JuanPerez@gmail.com" id="email" class="form_style-registro" type="email" required=""></input>
+            </div>
+            <div class="form_group-registro">
+                <label class="sub_title-registro" for="password">Contraseña</label>
+                <input placeholder="**********" id="password" class="form_style-registro" type="password"></input>
+            </div>
+            <div class="form_group-registro">
+                <label class="sub_title-registro" for="password">Confirmar Contraseña</label>
+                <input placeholder="**********" id="password" class="form_style-registro" type="password"></input>
+            </div>
+            <div>
+                <button class="btn-registro">REGISTRAR</button>
+                <p>Ya tienes una cuenta? <a class="link-registro" href="">Inicia Sesion</a></p><a class="link-registro" href="">
+            </a></div><a class="link-registro" href="">
+        
+    </a></form></div><a class="link-registro" href="">
+</a></div>
   );
 };
 
