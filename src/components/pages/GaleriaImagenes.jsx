2867301c26a77232c5645c2c26e1ac3a3a6a1f1c
Anto Ortega <antonellaportega@gmail.com>
2024-04-21 15:48:57 -0300

    formato con prettier

--- src/components/pages/GaleriaImagenes.jsx
+++ src/components/pages/GaleriaImagenes.jsx
@@ -19,12 +19,7 @@ import Modal12 from "../../assets/GaleriaImagenes/Modal12.jpg";
 import Modal13 from "../../assets/GaleriaImagenes/Modal13.jpg";
 import Modal14 from "../../assets/GaleriaImagenes/Modal14.jpg";
 
-import {
-  Modal,
-  ModalBody,
-  ModalFooter,
-  Button,
-} from "react-bootstrap";
+import { Modal, ModalBody, ModalFooter, Button } from "react-bootstrap";
 
 const GaleriaImagenes = () => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -38,7 +33,22 @@ const GaleriaImagenes = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [imagesPerPage] = useState(9); // Cantidad de imágenes por página
 
-  const modalImages = [Modal1, Modal4, Modal3,Modal2,Modal9, Modal5, Modal6, Modal7, Modal8,Modal10, Modal13, Modal14, Modal11, Modal12];
+  const modalImages = [
+    Modal1,
+    Modal4,
+    Modal3,
+    Modal2,
+    Modal9,
+    Modal5,
+    Modal6,
+    Modal7,
+    Modal8,
+    Modal10,
+    Modal13,
+    Modal14,
+    Modal11,
+    Modal12,
+  ];
 
   const indexOfLastImage = currentPage * imagesPerPage;
   const indexOfFirstImage = indexOfLastImage - imagesPerPage;
@@ -51,31 +61,11 @@ const GaleriaImagenes = () => {
         <h1>Galeria de Imagenes</h1>
       </section>
       <div className="galeria-moviles container my-3">
-        <img
-          src={movil1}
-          alt="logo Hotel Haki Wasi"
-          className="img-fluid"
-        ></img>
-        <img
-          src={movil2}
-          alt="logo Hotel Haki Wasi"
-          className="img-fluid"
-        ></img>
-        <img
-          src={movil3}
-          alt="logo Hotel Haki Wasi"
-          className="img-fluid"
-        ></img>
-        <img
-          src={Movil4}
-          alt="logo Hotel Haki Wasi"
-          className="img-fluid"
-        ></img>
-        <img
-          src={movil5}
-          alt="logo Hotel Haki Wasi"
-          className="img-fluid"
-        ></img>
+        <img src={movil1} alt="Vista de fuera" className="img-fluid"></img>
+        <img src={movil2} alt="Suite" className="img-fluid"></img>
+        <img src={movil3} alt="Cabañas" className="img-fluid"></img>
+        <img src={Movil4} alt="Pileta exterior" className="img-fluid"></img>
+        <img src={movil5} alt="Pileta Interior" className="img-fluid"></img>
       </div>
 
       <div className="container">
@@ -96,20 +86,41 @@ const GaleriaImagenes = () => {
       {/* Paginación */}
       <nav aria-label="Page navigation example">
         <ul className="pagination justify-content-center">
-          <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
-            <button className="page-link" onClick={() => paginate(currentPage - 1)}>
+          <li className={`page-item ${currentPage === 1 && "disabled"}`}>
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage - 1)}
+            >
               Previous
             </button>
           </li>
-          {[...Array(Math.ceil(modalImages.length / imagesPerPage)).keys()].map((number) => (
-            <li key={number} className={`page-item ${currentPage === number + 1 && 'active'}`}>
-              <button className="page-link" onClick={() => paginate(number + 1)}>
-                {number + 1}
-              </button>
-            </li>
-          ))}
-          <li className={`page-item ${currentPage === Math.ceil(modalImages.length / imagesPerPage) && 'disabled'}`}>
-            <button className="page-link" onClick={() => paginate(currentPage + 1)}>
+          {[...Array(Math.ceil(modalImages.length / imagesPerPage)).keys()].map(
+            (number) => (
+              <li
+                key={number}
+                className={`page-item ${
+                  currentPage === number + 1 && "active"
+                }`}
+              >
+                <button
+                  className="page-link"
+                  onClick={() => paginate(number + 1)}
+                >
+                  {number + 1}
+                </button>
+              </li>
+            )
+          )}
+          <li
+            className={`page-item ${
+              currentPage === Math.ceil(modalImages.length / imagesPerPage) &&
+              "disabled"
+            }`}
+          >
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage + 1)}
+            >
               Next
             </button>
           </li>
@@ -123,12 +134,22 @@ const GaleriaImagenes = () => {
           )}
         </ModalBody>
         <ModalFooter className="justify-content-center">
-        <button className="btn btn-dark d-block d-md-none" onClick={toggleModal}> {/* Botón de Bootstrap visible solo en dispositivos pequeños y medianos */}
-      Cerrar
-    </button>
-    <button className=" custom-button d-none d-md-inline-block" onClick={toggleModal}> {/* Botón personalizado visible solo en dispositivos grandes */}
-      Cerrar
-    </button>
+          <button
+            className="btn btn-dark d-block d-md-none"
+            onClick={toggleModal}
+          >
+            {" "}
+            {/* Botón de Bootstrap visible solo en dispositivos pequeños y medianos */}
+            Cerrar
+          </button>
+          <button
+            className=" custom-button d-none d-md-inline-block"
+            onClick={toggleModal}
+          >
+            {" "}
+            {/* Botón personalizado visible solo en dispositivos grandes */}
+            Cerrar
+          </button>
         </ModalFooter>
       </Modal>
     </>
