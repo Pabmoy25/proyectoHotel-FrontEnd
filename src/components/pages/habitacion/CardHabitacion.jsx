4c4f6b3bf5148bddfec01a6b429adad45b727d6b
Pabmoy25 <pabmoy1725@gmail.com>
2024-04-20 21:58:49 -0300

    se realiza el maquetado inicial y la logica para ver cardHabitacion en la pagina principal

--- src/components/pages/habitacion/CardHabitacion.jsx
+++ src/components/pages/habitacion/CardHabitacion.jsx
@@ -1,11 +1,26 @@
+import { Col, Card} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-
-const CardHabitacion = () => {
+const CardHabitacion = ({cardHabitacion}) => {
     return (
-        <div>
-            
-        </div>
-    );
-};
+        <Col md={4} lg={3} className="mb-3">
+          <Card className="h-100">
+            <div>
+              <img src={cardHabitacion.imagen} alt={cardHabitacion.tipoDeHabitacion} className="card-img-top-nueva" />
+            </div>
+            <Card.Body>
+            <Card.Title>Habitacion: {cardHabitacion.tipoDeHabitacion}</Card.Title>
+            <Card.Text>
+            {cardHabitacion.descripcion_breve}<br className="mb-2"/> 
+             </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <Link className='btn btn-secondary me-2' to={""}>Ver Habitación</Link>
+            </Card.Footer>
+          </Card>
+        </Col>
+      );
+    };
+
 
 export default CardHabitacion;
