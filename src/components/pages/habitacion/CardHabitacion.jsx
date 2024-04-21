3e6ed86e58a02394477e1b89d49663acb7f410c7
Pabmoy25 <pabmoy1725@gmail.com>
2024-04-20 22:28:22 -0300

    se realiza el maquetado en bordes de imagenes y se coloca clase container

--- src/components/pages/habitacion/CardHabitacion.jsx
+++ src/components/pages/habitacion/CardHabitacion.jsx
@@ -1,26 +1,24 @@
-import { Col, Card} from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const CardHabitacion = ({cardHabitacion}) => {
     return (
-        <Col md={4} lg={3} className="mb-3">
-          <Card className="h-100">
-            <div>
-              <img src={cardHabitacion.imagen} alt={cardHabitacion.tipoDeHabitacion} className="card-img-top-nueva" />
-            </div>
-            <Card.Body>
-            <Card.Title>Habitacion: {cardHabitacion.tipoDeHabitacion}</Card.Title>
+        
+        <Col md={4} lg={3} className="my-4">
+    <Card className="h-100 card-custom-rounded">
+        <div className="container">
+            <img src={cardHabitacion.imagen} alt={cardHabitacion.tipoDeHabitacion} className="card-img-top-nueva redondearImgCard" />
+        </div>
+        <Card.Body>
+            <Card.Title>Habitación: {cardHabitacion.tipoDeHabitacion}</Card.Title>
             <Card.Text>
-            {cardHabitacion.descripcion_breve}<br className="mb-2"/> 
-             </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-            <Link className='btn btn-secondary me-2' to={""}>Ver Habitación</Link>
-            </Card.Footer>
-          </Card>
-        </Col>
+                {cardHabitacion.descripcion_breve}<br className="mb-2"/> 
+            </Card.Text>
+        </Card.Body>
+        <Link className='btn btn-secondary me-2' to={""}>Ver Habitación</Link>
+    </Card>
+</Col>
       );
     };
-
 
 export default CardHabitacion;
