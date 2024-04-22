7ff8044d4a0c8ee9238a9ff37dfa2dd6655fe9fc
Pabmoy25 <pabmoy1725@gmail.com>
2024-04-21 22:11:01 -0300

    se corrige el tamaño de los iconos de servicio del hotel en la paginaPrincipal

--- src/components/pages/PaginaPrincipal/CardOK.jsx
+++ src/components/pages/PaginaPrincipal/CardOK.jsx
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const CardOK = ({ title, imageURL }) => {
   return (
-    <div className="card text-center mx-auto">
+    <div className="cardok text-center mx-auto">
       <img src={imageURL} className="card-image" alt="icono" />
       <div className="card-body">
         <h3 className="card-title text-center">{title}</h3>
