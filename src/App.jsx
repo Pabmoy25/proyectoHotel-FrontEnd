56642197a8107781eb4c57ca8914c226e7eb44fb
Anto Ortega <antonellaportega@gmail.com>
2024-04-22 13:42:37 -0300

    se borro unas lineas que daban error

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ import CatalogoHabitacion from "./components/pages/habitacion/CatalogoHabitacion
 import Registro from "./components/pages/Registro"
 
 import Contacto from "./components/pages/Contacto";
-=======
 import Propuestas from "./components/pages/Propuestas";
 import FormularioHabitacion from "./components/pages/habitacion/FormularioHabitacion";
 
