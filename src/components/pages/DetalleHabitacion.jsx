66da3ef90bdbd66a9668685163af5b41121f6b73
Agostina Bellotti <agostinabell@gmail.com>
2024-04-22 13:08:30 -0300

    Se agregó un alert y se rellenó el return

--- src/components/pages/DetalleHabitacion.jsx
+++ src/components/pages/DetalleHabitacion.jsx
@@ -13,11 +13,52 @@ import Swal from "sweetalert2";
     cargarDetalle();
   },[])
 
+  const cargarDetalle = async()=>{
+   const respuesta = await obtenerHabitacion(id)
+   if(respuesta.status === 200){
+     //mostrar el producto en la card
+     const datoHabitacion = await respuesta.json();
+     setHabitacion(datoHabitacion);
+   }else{
+     Swal.fire({
+       title: "Ocurrio un error",
+       text: "Intente realizar esta operacion en unos minutos",
+       icon: "error",
+     });
+   }
+ }
 
  return (
-        <div>
-            
-        </div>
+      
+   <Container className="my-3 mainpage">
+      <Card>
+        <Row>
+          <Col md={6}>
+            <Card.Img
+              variant="top"
+              src={habitacion.imagen}
+              alt={habitacion.nombrehabitacion}
+              className="card-img-top-detalle" 
+              />
+          </Col>
+          <Col md={6}>
+            <Card.Body>
+              <Card.Title className="primary-font"><h3>{habitacion.nombrehabitacion}</h3></Card.Title>
+              <hr />
+              <Card.Text>
+              {habitacion.descripcion_amplia}
+              <br/>
+              <br/>
+              <span className="primary-font fw-semibold ">Categoria:${habitacion.categoria} </span> 
+              <br className='mb-3'/>
+              <span className="primary-font fw-semibold ">Precio: ${habitacion.precio}</span>
+            </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
+      
     );
  };
  
