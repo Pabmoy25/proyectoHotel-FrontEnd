94e7ceaa85ed51aac18162c054b5b520f41a6f8b
Antonella Ortega <antonellaportega@gmail.com>
2024-04-23 02:51:04 -0300

    formato con prettier

--- src/components/pages/Reservas/DatePicker.jsx
+++ src/components/pages/Reservas/DatePicker.jsx
@@ -1,45 +1,44 @@
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { DateRange } from 'react-date-range';
-import { useState } from 'react';
-
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRange } from "react-date-range";
+import { useState } from "react";
 
 const DatePicker = () => {
-    const [state, setState] = useState([
-      {
-        startDate: new Date(),
-        endDate: null,
-        key: 'selection'
-      }
-    ]);
-    const [showDatePicker, setShowDatePicker] = useState(false);
-  
-    const toggleDatePicker = () => {
-      setShowDatePicker(!showDatePicker);
-    };
-  
-    const handleDateChange = (item) => {
-      setState([item.selection]);
-    };
-  
-    return (
-      <div className="date-picker-container">
-        <button className="reservas-button" onClick={toggleDatePicker}>
-          Haz Click para Reservar
-        </button>
-        {showDatePicker && (
-          <div className="date-range-container">
-            <DateRange
-              editableDateInputs={true}
-              onChange={handleDateChange}
-              moveRangeOnFirstSelection={false}
-              ranges={state}
-            />
-            <button className="search-button">Buscar Habitaciones</button>
-          </div>
-        )}
-      </div>
-    );
+  const [state, setState] = useState([
+    {
+      startDate: new Date(),
+      endDate: null,
+      key: "selection",
+    },
+  ]);
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const toggleDatePicker = () => {
+    setShowDatePicker(!showDatePicker);
   };
-  
-  export default DatePicker;
+
+  const handleDateChange = (item) => {
+    setState([item.selection]);
+  };
+
+  return (
+    <div className="date-picker-container">
+      <button className="reservas-button" onClick={toggleDatePicker}>
+        Haz Click para Reservar
+      </button>
+      {showDatePicker && (
+        <div className="date-range-container">
+          <DateRange
+            editableDateInputs={true}
+            onChange={handleDateChange}
+            moveRangeOnFirstSelection={false}
+            ranges={state}
+          />
+          <button className="search-button">Buscar Habitaciones</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DatePicker;
--- src/components/pages/habitacion/CardHabitacion.jsx
+++ src/components/pages/habitacion/CardHabitacion.jsx
@@ -1,25 +1,30 @@
 import { Col, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-
-const CardHabitacion = ({cardHabitacion}) => {
-    return (
-        
-        <Col md={4} lg={3} className="my-4">
-    <Card className="h-100 card-custom-rounded">
+const CardHabitacion = ({ cardHabitacion }) => {
+  return (
+    <Col md={4} lg={3} className="my-4">
+      <Card className="h-100 card-custom-rounded">
         <div className="container">
-            <img src={cardHabitacion.imagen} alt={cardHabitacion.tipoDeHabitacion} className="card-img-top-nueva redondearImgCard" />
+          <img
+            src={cardHabitacion.imagen}
+            alt={cardHabitacion.tipoDeHabitacion}
+            className="card-img-top-nueva redondearImgCard"
+          />
         </div>
         <Card.Body>
-            <Card.Title>Habitación: {cardHabitacion.tipoDeHabitacion}</Card.Title>
-            <Card.Text>
-                {cardHabitacion.descripcion_breve}<br className="mb-2"/> 
-            </Card.Text>
+          <Card.Title>Habitación: {cardHabitacion.tipoDeHabitacion}</Card.Title>
+          <Card.Text>
+            {cardHabitacion.descripcion_breve}
+            <br className="mb-2" />
+          </Card.Text>
         </Card.Body>
-        <Link className='btn btn-secondary me-2' to={""}>Ver Habitación</Link>
-    </Card>
-</Col>
-      );
-    };
+        <Link className="btn btn-secondary me-2" to={""}>
+          Ver Habitación
+        </Link>
+      </Card>
+    </Col>
+  );
+};
 
 export default CardHabitacion;
