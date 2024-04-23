e7ad26b9f95154d81484055c423ed02fa6f0fa2a
Agostina Bellotti <agostinabell@gmail.com>
2024-04-23 12:22:00 -0300

    Se realizaron modificaciones quitando funciones y agregando otros elementos como el carrusel, imagenes desde url y divs con contenido

--- src/components/pages/DetalleHabitacion.jsx
+++ src/components/pages/DetalleHabitacion.jsx
@@ -1,65 +1,91 @@
-import { useEffect, useState } from "react";
-import { Container, Card, Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
-import { obtenerHabitacion } from "../../helpers/queriesHabitaciones";
+import { Container, Row, Col } from "react-bootstrap";
 import Swal from "sweetalert2";
+import {Carousel} from "react-bootstrap";
+
  
  const DetalleHabitacion = () => {
- const {id}= useParams ();
- const [habitacion, setHabitacion] = useState({});
-
- useEffect(()=>{
-    //buscar el producto que quiero maquetar
-    cargarDetalle();
-  },[])
-
-  const cargarDetalle = async()=>{
-   const respuesta = await obtenerHabitacion(id)
-   if(respuesta.status === 200){
-     //mostrar el producto en la card
-     const datoHabitacion = await respuesta.json();
-     setHabitacion(datoHabitacion);
-   }else{
-     Swal.fire({
-       title: "Ocurrio un error",
-       text: "Intente realizar esta operacion en unos minutos",
-       icon: "error",
-     });
-   }
- }
-
  return (
-      
- <Container className="my-3 mainpage">
-   <h1>DETALLE HABITACION</h1>
-      <Card>
+  <>
+   <div className="portadaHabitaciones mb-5 text-center py-1">
+
+  <div className=" Banner Pagina Detalles py-1"/>
+  <div>
+    <h1 className="Titulo detalles text-white"> DETALLES DE LA HABITACIÓN</h1>
+  </div>
+ </div>
+
+ <Container className="mt-5">
         <Row>
           <Col md={6}>
-            <Card.Img
-              variant="top"
-              src={habitacion.imagen}
-              alt={habitacion.nombrehabitacion}
-              className="card-img-top-detalle" 
-              />
-          </Col>
-          <Col md={6}>
-            <Card.Body>
-              <Card.Title className="primary-font"><h3>{habitacion.nombrehabitacion}</h3></Card.Title>
-              <hr />
-              <Card.Text>
-              {habitacion.descripcion_amplia}
-              <br/>
-              <br/>
-              <span className="primary-font fw-semibold ">Categoria:${habitacion.categoria} </span> 
-              <br className='mb-3'/>
-              <span className="primary-font fw-semibold ">Precio: ${habitacion.precio}</span>
-            </Card.Text>
-            </Card.Body>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/7623568/pexels-photo-7623568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/14859259/pexels-photo-14859259.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="Second slide"
+                />
+             </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.pexels.com/photos/15195270/pexels-photo-15195270/free-photo-of-relajacion-hotel-cama-habitacion.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  />
+              </Carousel.Item>
+            </Carousel>
           </Col>
         </Row>
-      </Card>
-    </Container>
+        <Col md={6}className="order-md-last mt-md- mt-4">
+            <div>
+              <h2 className="Titulo Pregunta text-center">¿Qué incluye la habitación?</h2>
+              <p className="parrafo amenities text-start ">Hidromasaje para 2 personas, Escritorios de trabajo, Diseño exclusivo, Personal bar, Cafetera Nespresso, Batas y pantuflas, 2 Smart TV, Teléfono inalámbrico, Conexión wi-fi sin cargo, Secador de pelo, Plancha de pelo (a pedido), Amenities exclusivos, Caja de seguridad para notebook, Control de temperatura individual, Equipo de yoga (a pedido). Mate & Tea Kit. Accessorios varios para mascotas.</p>
+            </div>
+          </Col>
+         
+          <div className="mt-4">
+              <h3>Servicios generales</h3>
+              <Row>
+                <Col xs={6} sm={3}>
+                  <div>
+                    <i className="fa fa-bed"></i>
+                    <p>Cama</p>
+                  </div>
+                </Col>
+                <Col xs={6} sm={3}>
+                  <div>
+                    <i className="fa fa-wifi"></i>
+                    <p>WiFi</p>
+                  </div>
+                </Col>
+                <Col xs={6} sm={3}>
+                  <div>
+                    <i className="fa fa-television"></i>
+                    <p>TV</p>
+                  </div>
+                </Col>
+                <Col xs={6} sm={3}>
+                  <div>
+                    <i className="fa fa-coffee"></i>
+                    <p>Café</p>
+                  </div>
+                </Col>
+              </Row>
+            </div>
       
+      </Container>
+  
+</>
+  
+
+   
     );
  };
  
