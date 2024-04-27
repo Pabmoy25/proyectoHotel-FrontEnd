b3f6302d02bc218fc9defb2954f9c86f8360ec88
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-04-27 20:56:33 -0300

    se agrego un acordeon a la pagina principal

---
+++ src/components/pages/Reservas/Reserva.jsx
@@ -0,0 +1,28 @@
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+const Reserva = ({reserva}) => {
+    return (
+        <tr>
+          <td className="text-center">{reserva.nombreCompleto}</td>
+          <td className="text-center">{reserva.email}</td>
+          <td className="d-flex justify-content-center">
+            <Link
+              className="btn"
+              id="btnEditar"
+              to={`/registro/editar/${reserva._id}`}
+            >
+              <i className="bi bi-pencil-square"></i>
+            </Link>
+            <Button id="btnBorrar">
+              {" "}
+              {/*</td>onClick={borrarReservas}>*/}
+              <i className="bi bi-trash-fill"></i>
+            </Button>
+          </td>
+        </tr>
+      );
+    };
+
+export default Reserva;
