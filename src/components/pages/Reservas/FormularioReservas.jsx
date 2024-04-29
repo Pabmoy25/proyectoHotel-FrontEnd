5d071cbb034295ac3a62f1f28ae215d57fbc6ac0
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-04-29 08:35:28 -0300

    se realizo el maquetado inicial del formulario reserva

--- src/components/pages/Reservas/FormularioReservas.jsx
+++ src/components/pages/Reservas/FormularioReservas.jsx
@@ -5,53 +5,50 @@ import Swal from "sweetalert2";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import DatePicker from "react-datepicker";
+import { crearReservas } from "../../../helpers/queriesReserva";
 
 const FormularioReservas = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
-    
-  
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-        reset
-      } = useForm();   
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-      const reservaValidada = async(reserva) => {
-        const respuesta = await crearReservas(reserva);
-        if(respuesta.status === 201){
-          Swal.fire({
-            title: "reserva creada",
-            text: `La reserva fue creada con exito`,
-            icon: "success",
-          });
-          reset();
-        }else{
-          Swal.fire({
-            title: "Ocurrio un error",
-            text: "Intente crear la reserva en unos minutos",
-            icon: "error",
-          });
-        }
-      };
-   
-    
-        return (
-        <>
-          <div className="container-registro Background-registro">
-            <Form
-              className="custom-form rounded my-3 w-50"
-              onSubmit={handleSubmit(reservaValidada)}
-              id="formHabitacion"
-              
-            >
-              <div>
-                <p className="title-registro">Reservar</p>
-              </div>
-              <Row>
-              <Col md={6}>
+  const reservaValidada = async (reserva) => {
+    const respuesta = await crearReservas(reserva);
+    if (respuesta.status === 201) {
+      Swal.fire({
+        title: "reserva creada",
+        text: `La reserva fue creada con exito`,
+        icon: "success",
+      });
+      reset();
+    } else {
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: "Intente crear la reserva en unos minutos",
+        icon: "error",
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className="container-registro Background-registro">
+        <Form
+          className="custom-form rounded my-3 w-50"
+          onSubmit={handleSubmit(reservaValidada)}
+          id="formHabitacion"
+        >
+          <div>
+            <p className="title-registro">Reservar</p>
+          </div>
+          <Row>
+            <Col md={4}>
               <Form.Group className="mb-3" controlId="formNombre">
                 <Form.Label className="sub_title-registro">
                   Nombre y apellido
@@ -76,8 +73,8 @@ const FormularioReservas = () => {
                   {errors.nombreCompleto?.message}
                 </Form.Text>
               </Form.Group>
-              </Col>
-              <Col md={6}>
+            </Col>
+            <Col md={4}>
               <Form.Group className="mb-3" controlId="formEmail">
                 <Form.Label className="sub_title-registro">E-mail</Form.Label>
                 <Form.Control
@@ -96,7 +93,7 @@ const FormularioReservas = () => {
                     },
                     pattern: {
                       value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-    
+
                       message: "Ingresar dirección de mail válida",
                     },
                   })}
@@ -105,88 +102,169 @@ const FormularioReservas = () => {
                   {errors.email?.message}
                 </Form.Text>
               </Form.Group>
-              </Col>
-             <Col md={6}>
-             <Form.Group className="mb-3" controlId="formRegistroTelefono">
-              <Form.Label className="sub_title-registro">Número de Contacto:</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Ej: 3816810178"
-                {...register("telefono", {
-                  required: "El telefono de contacto es obligatorio",
-                  pattern: {
-                    value: /^[0-9]{10}$/,
-                    message:
-                      "Ingresa un número de teléfono válido (10 dígitos)",
-                  },
-                })}
-              />
-              <Form.Text className="text-danger">
-                {errors.telefono?.message}
-              </Form.Text>
-            </Form.Group>
-                </Col>
+            </Col>
+            <Col md={4}>
+              <Form.Group className="mb-3" controlId="formRegistroTelefono">
+                <Form.Label className="sub_title-registro">
+                  Número de Contacto:
+                </Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Ej: 3816810178"
+                  {...register("telefono", {
+                    required: "El telefono de contacto es obligatorio",
+                    pattern: {
+                      value: /^[0-9]{10}$/,
+                      message:
+                        "Ingresa un número de teléfono válido (10 dígitos)",
+                    },
+                  })}
+                />
+                <Form.Text className="text-danger">
+                  {errors.telefono?.message}
+                </Form.Text>
+              </Form.Group>
+            </Col>
             <Row>
-          <Col md={4}>
-            <Form.Group className="mb-3" controlId="formFechaEntrada">
-              <Form.Label className="sub_title-registro">Fecha de Entrada</Form.Label>
-              <DatePicker
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                selectsStart
-                startDate={startDate}
-                endDate={endDate}
-                dateFormat="dd/MM/yyyy"
-                className="form-control text-center"
-              />
-            </Form.Group>
-          </Col>
-          <Col md={4}>
-            <Form.Group className="mb-3" controlId="formFechaSalida">
-              <Form.Label className="sub_title-registro">Fecha de Salida</Form.Label>
-              <DatePicker
-                selected={endDate}
-                onChange={(date) => setEndDate(date)}
-                selectsEnd
-                startDate={startDate}
-                endDate={endDate}
-                minDate={startDate}
-                dateFormat="dd/MM/yyyy"
-                className="form-control text-center"
-              />
-            </Form.Group>
-          </Col>
-          <Col md={4}>
-            <Form.Group className="mb-3" controlId="formFechaSalida">
-              <Form.Label className="sub_title-registro">Total de días</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Ej: 7"
-                {...register("TotalDeDias", {
-                  required: "Elija un periodo de días",
-                  pattern: {
-                    value: /^[0-9]{3}$/,
-                    message:
-                      "Los dias no pueden superar los 3 dígitos)",
-                  },
-                })}
-              />
-              <Form.Text className="text-danger">
-                {errors.telefono?.message}
-              </Form.Text>
-            </Form.Group>
-          </Col>
-        </Row>
-              </Row>
-                <div className="d-flex justify-content-center">
-                <Button type="submit" className="mb-3" id="btn-registro">
-                  Confirmar
-                </Button>
-              </div>
-            </Form>
+              <Col md={4}>
+                <Form.Group className="mb-3" controlId="formNumeroHabitacion">
+                  <Form.Label className="sub_title-registro">
+                    Numero de Habitación*
+                  </Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="A101"
+                    {...register("habitacion", {
+                      required: "El número de habitacion es obligatorio",
+                      minLength: {
+                        value: 2,
+                        message:
+                          "Debe ingresar como minimo 2 caracteres para el número de habitación",
+                      },
+                      maxLength: {
+                        value: 5,
+                        message:
+                          "Debe ingresar como maximo 5 caracteres para el número de habitación",
+                      },
+                    })}
+                  />
+                  <Form.Text className="text-danger">
+                    {errors.habitacion?.message}
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col md={4}>
+                <Form.Group className="mb-3" controlId="formTipo">
+                  <Form.Label className="sub_title-registro">
+                    Tipo de Habitación*
+                  </Form.Label>
+                  <Form.Select
+                    {...register("tipoDeHabitacion", {
+                      required: "Debe seleccionar una categoria de habitación",
+                    })}
+                  >
+                    <option value="">Seleccione una opcion</option>
+                    <option value="Estándar">Estándar</option>
+                    <option value="Doble">Doble</option>
+                    <option value="Deluxe">Deluxe</option>
+                    <option value="Suite">Suite</option>
+                  </Form.Select>
+                  <Form.Text className="text-danger">
+                    {errors.tipoDeHabitacion?.message}
+                  </Form.Text>
+                </Form.Group>
+                </Col>
+                <Col md={4}>
+                  <Form.Group className="mb-3" controlId="formPrecio">
+                    <Form.Label className="sub_title-registro">Tarifa por noche*</Form.Label>
+                    <Form.Control
+                      type="number"
+                      placeholder="20000"
+                      {...register("precio", {
+                        required: "El precio es obligatorio",
+                        min: {
+                          value: 10000,
+                          message: "El precio minimo es de $10.000",
+                        },
+                        max: {
+                          value: 100000,
+                          message: "El precio maximo es de $100.000",
+                        },
+                      })}
+                    />
+                    <Form.Text className="text-danger">
+                      {errors.precio?.message}
+                    </Form.Text>
+                  </Form.Group>
+              
+              </Col>
+            </Row>
+            <Row>
+              <Col md={4}>
+                <Form.Group className="mb-3" controlId="formFechaEntrada">
+                  <Form.Label className="sub_title-registro">
+                    Fecha de Entrada
+                  </Form.Label>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    dateFormat="dd/MM/yyyy"
+                    className="form-control text-center"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={4}>
+                <Form.Group className="mb-3" controlId="formFechaSalida">
+                  <Form.Label className="sub_title-registro">
+                    Fecha de Salida
+                  </Form.Label>
+                  <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate}
+                    dateFormat="dd/MM/yyyy"
+                    className="form-control text-center"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={4}>
+                <Form.Group className="mb-3" controlId="formFechaSalida">
+                  <Form.Label className="sub_title-registro">
+                    Total de días
+                  </Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Ej: 7"
+                    {...register("TotalDeDias", {
+                      required: "Elija un periodo de días",
+                      pattern: {
+                        value: /^[0-9]{3}$/,
+                        message: "Los dias no pueden superar los 3 dígitos)",
+                      },
+                    })}
+                  />
+                  <Form.Text className="text-danger">
+                    {errors.telefono?.message}
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+            </Row>
+          </Row>
+          <div className="d-flex justify-content-center">
+            <Button type="submit" className="mb-3" id="btn-registro">
+              Confirmar
+            </Button>
           </div>
-        </>
-      );
-    };
+        </Form>
+      </div>
+    </>
+  );
+};
 
 export default FormularioReservas;
