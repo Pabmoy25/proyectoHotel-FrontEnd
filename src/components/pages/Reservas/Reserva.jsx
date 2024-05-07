71809a38e386637be38ba8bda2deb019ca156d20
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-07 08:41:44 -0300

    se creo la logica para borrar una reserva en el componente reserva y el boton correpondiente para ejecutarla

--- src/components/pages/Reservas/Reserva.jsx
+++ src/components/pages/Reservas/Reserva.jsx
@@ -1,8 +1,40 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { borrarReserva } from "../../../helpers/queriesReserva";
+import Swal from "sweetalert2";
 
 
-const Reserva = ({reserva}) => {
+const Reserva = ({reserva, eliminarReserva}) => {
+  const borrarReservas = () => {
+    Swal.fire({
+      title: "¿Está seguro de eliminar esta reserva?",
+      text: "No puedes revertir este proceso",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Borrar",
+      cancelButtonText: "Cancelar",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const respuesta = await borrarReserva(reserva._id);
+        if (respuesta.status === 200) {
+          Swal.fire({
+            title: "La reserva fue eliminada",
+            text: `La reserva fue eliminada`,
+            icon: "success",
+          });
+          eliminarReserva(reserva._id);
+        } else {
+          Swal.fire({
+            title: "Ocurrió un error",
+            text: `La reserva no fue eliminada, intentelo de nuevo en unos minutos`,
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
     return (
         <tr>
           <td className="text-center">{reserva.habitacion}</td>
@@ -22,7 +54,7 @@ const Reserva = ({reserva}) => {
             >
               <i className="bi bi-pencil-square"></i>
             </Link>
-            <Button id="btnBorrar">
+            <Button id="btnBorrar"  onClick={borrarReservas}>
               {" "}
               {/*</td>onClick={borrarReservas}>*/}
               <i className="bi bi-trash-fill"></i>
