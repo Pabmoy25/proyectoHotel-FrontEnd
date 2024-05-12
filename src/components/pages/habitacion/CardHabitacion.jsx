40391f2573af9b78fc3db8068f56fc7dec1b1b0e
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-12 11:43:05 -0300

    se modifico el maquetado del boton para crear una reserva

--- src/components/pages/habitacion/CardHabitacion.jsx
+++ src/components/pages/habitacion/CardHabitacion.jsx
@@ -28,62 +28,58 @@ const CardHabitacion = ({ cardHabitacion }) => {
     ? "Habitación reservada"
     : "Reservar";
 
-  return (
-    <Col md={4} lg={3} className="my-4 d-flex justify-content-center">
-      <div className="card-container">
-        <Card className="h-100 card-custom-rounded">
-          <div className="container">
-            <img
-              src={cardHabitacion.imagen}
-              alt={cardHabitacion.tipoDeHabitacion}
-              className="card-img-top-nueva redondearImgCard"
-            />
-          </div>
-          <Card.Body>
-            <Card.Title>
-              Habitación: {cardHabitacion.tipoDeHabitacion}
-            </Card.Title>
-            <Card.Text>
-              {cardHabitacion.descripcion_breve}
-              <br className="mb-2" />
-            </Card.Text>
-          </Card.Body>
-          <Link
-            className="search-button "
-            to={"/detalleHabitacion/" + cardHabitacion._id}
-          >
-            Ver Habitación
-          </Link>
-          {/* <Link
-            className="search-button mt-2"
-            to={`/reserva/${cardHabitacion._id}`}
-            disabled={habitacionReservada}
-          >
-            {mensajeReserva}
-          </Link> */}
-
-          <div className="text-center">
-            {habitacionReservada ? (
-              <Button
-                className="opacity-25 mt-3 border-0"
-                variant="dark"
-                disabled
-              >
-                No disponible
-              </Button>
-            ) : (
-              <Link
-                className="btn btn-inicio-card mt-2 border-0"
-                variant="dark"
-                to={`/reserva/${cardHabitacion._id}`}
-              >
-                Reservar
-              </Link>
-            )}
-          </div>
-        </Card>
-      </div>
-    </Col>
+    return (
+      <Col md={4} lg={3} className="my-4 d-flex justify-content-center">
+        <div className="card-container">
+          <Card className="h-100 card-custom-rounded">
+            <div className="container">
+              <img
+                src={cardHabitacion.imagen}
+                alt={cardHabitacion.tipoDeHabitacion}
+                className="card-img-top-nueva redondearImgCard"
+              />
+            </div>
+            <Card.Body>
+              <Card.Title>
+                Habitación: {cardHabitacion.tipoDeHabitacion}
+              </Card.Title>
+              <Card.Text>
+                {cardHabitacion.descripcion_breve}
+                <br className="mb-2" />
+              </Card.Text>
+            </Card.Body>
+            <Link
+              className="search-button text-decoration-none"
+              to={"/detalleHabitacion/" + cardHabitacion._id}
+            >
+              Ver Habitación
+            </Link>
+    
+            <div className="text-center">
+              {habitacionReservada ? (
+                <Button
+                  className="opacity-50 mt-3 border-0"
+                  variant="dark"
+                  disabled
+                >
+                  No disponible
+                </Button>
+              ) : (
+                <Button
+                className="mt-3"
+              variant="outline-secondary"
+              id="btnAdmin"
+              as={Link}
+              to={`/reserva/${cardHabitacion._id}`}
+            >
+              <i className="bi bi-file-earmark-plus"> Reservar</i>
+            </Button>
+              )}
+            </div>
+           
+          </Card>
+        </div>
+      </Col>
   );
 };
 
