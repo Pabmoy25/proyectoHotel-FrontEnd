85987c924c943be4acda1e9231fd39731fe65a7a
Pablo Moyano <101376798+Pabmoy25@users.noreply.github.com>
2024-05-12 12:03:40 -0300

    se da formato al codigo de CardHabitacion y se elimina los comentarios

--- src/components/pages/habitacion/CardHabitacion.jsx
+++ src/components/pages/habitacion/CardHabitacion.jsx
@@ -18,68 +18,62 @@ const CardHabitacion = ({ cardHabitacion }) => {
     traerReservas();
   }, []);
 
-  // Verificar si la habitación está reservada
   const habitacionReservada = reservas.some(
     (reserva) => reserva.habitacion === cardHabitacion.habitacion
   );
   console.log(habitacionReservada);
 
-  const mensajeReserva = habitacionReservada
-    ? "Habitación reservada"
-    : "Reservar";
+  return (
+    <Col md={4} lg={3} className="my-4 d-flex justify-content-center">
+      <div className="card-container">
+        <Card className="h-100 card-custom-rounded">
+          <div className="container">
+            <img
+              src={cardHabitacion.imagen}
+              alt={cardHabitacion.tipoDeHabitacion}
+              className="card-img-top-nueva redondearImgCard"
+            />
+          </div>
+          <Card.Body>
+            <Card.Title>
+              Habitación: {cardHabitacion.tipoDeHabitacion}
+            </Card.Title>
+            <Card.Text>
+              {cardHabitacion.descripcion_breve}
+              <br className="mb-2" />
+            </Card.Text>
+          </Card.Body>
+          <Link
+            className="search-button text-decoration-none"
+            to={"/detalleHabitacion/" + cardHabitacion._id}
+          >
+            Ver Habitación
+          </Link>
 
-    return (
-      <Col md={4} lg={3} className="my-4 d-flex justify-content-center">
-        <div className="card-container">
-          <Card className="h-100 card-custom-rounded">
-            <div className="container">
-              <img
-                src={cardHabitacion.imagen}
-                alt={cardHabitacion.tipoDeHabitacion}
-                className="card-img-top-nueva redondearImgCard"
-              />
-            </div>
-            <Card.Body>
-              <Card.Title>
-                Habitación: {cardHabitacion.tipoDeHabitacion}
-              </Card.Title>
-              <Card.Text>
-                {cardHabitacion.descripcion_breve}
-                <br className="mb-2" />
-              </Card.Text>
-            </Card.Body>
-            <Link
-              className="search-button text-decoration-none"
-              to={"/detalleHabitacion/" + cardHabitacion._id}
-            >
-              Ver Habitación
-            </Link>
-    
-            <div className="text-center">
-              {habitacionReservada ? (
-                <Button
-                  className="opacity-50 mt-3 border-0"
-                  variant="dark"
-                  disabled
-                >
-                  No disponible
-                </Button>
-              ) : (
-                <Button
+          <div className="text-center">
+            {habitacionReservada ? (
+              <Button
+                className="opacity-50 mt-3 border-0"
+                variant="dark"
+                disabled
+              >
+                No disponible
+              </Button>
+            ) : (
+              <Button
                 className="mt-3"
-              variant="outline-secondary"
-              id="btnAdmin"
-              as={Link}
-              to={`/reserva/${cardHabitacion._id}`}
-            >
-              <i className="bi bi-file-earmark-plus"> Reservar</i>
-            </Button>
-              )}
-            </div>
-           
-          </Card>
-        </div>
-      </Col>
+                variant="outline-secondary"
+                id="btnAdmin"
+                as={Link}
+                to={`/reserva/${cardHabitacion._id}`}
+              >
+                <i className="bi bi-file-earmark-plus"> Reservar</i>
+              </Button>
+            )}
+          </div>
+        </Card>
+      </div>
+    </Col>
   );
 };
 
